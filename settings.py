102d497b6d0003f68fc4187736edb067d9a63336
Paweł <pryzwanpawel96@gmail.com>
2021-01-14 16:43:50 +0100

    Refactoring

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 """ Settings for game"""
 
-SERVER = "127.0.0.1"  # Server
-PORT = 5555         # PORT
-BOARD_SIZE = 10     # Board Size for example 10x10
-LEN_SHIPS = [1]   # List of length of ships
+SERVER = "192.168.0.38"  # "127.0.0.1" if you want to test it on localhost
+PORT = 5555  # PORT
+BOARD_SIZE = 10  # Board Size for example 10x10
+LEN_SHIPS = [1]  # List of length of ships
